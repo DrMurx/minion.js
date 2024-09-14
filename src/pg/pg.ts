edbae5c6dce5cd65add1f153009365f39f2d2e95
Jan Kunzmann <jan-github@phobia.de>
2024-09-15 01:13:59 +0200

    Refactor config parser into Pg class

--- src/pg/pg.ts
+++ src/pg/pg.ts
@@ -1,7 +1,6 @@
 import EventEmitter from 'events';
 import {Connection} from './connection.js';
 import {Results} from './results.js';
-import {parseConfig} from './config.js';
 import {throwWithContext} from './util.js';
 import pg from 'pg';
 
@@ -16,9 +15,42 @@ export class Pg extends EventEmitter {
 
   constructor(config: string) {
     super();
-    this.pool = new pg.Pool({allowExitOnIdle: true, ...parseConfig(config)});
+    this.pool = new pg.Pool({allowExitOnIdle: true, ...Pg.parseConfig(config)});
     // Convert BIGINT to number (even if not all 64bit are usable)
     pg.types.setTypeParser(20, parseInt);
+  }
+
+  /**
+   * Parse PostgreSQL connection URI.
+   */
+  static parseConfig(config: string): pg.PoolConfig {
+    const url = new URL(config);
+    if (url.protocol !== 'postgres:' && url.protocol !== 'postgresql:') {
+      throw new TypeError(`Invalid URL: ${config}`);
+    }
+
+    const poolConfig: pg.PoolConfig = {};
+    if (url.hostname !== '') {
+      poolConfig.host = decodeURIComponent(url.hostname);
+    }
+    if (url.port !== '') {
+      poolConfig.port = parseInt(url.port);
+    }
+    if (url.username !== '') {
+      poolConfig.user = url.username;
+    }
+    if (url.password !== '') {
+      poolConfig.password = url.password;
+    }
+    if (url.pathname.startsWith('/')) {
+      poolConfig.database = decodeURIComponent(url.pathname.slice(1));
+    }
+    const currentSchema = url.searchParams.get('currentSchema');
+    if (currentSchema !== null) {
+      poolConfig.options = `-c search_path=${currentSchema}`;
+    }
+
+    return poolConfig;
   }
 
   async [Symbol.asyncDispose]() {
